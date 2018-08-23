7a1773f2107b54cfe2c17ca425e4cc47fb7c81d3
Bono <bono@tuta.io>
2018-08-24 00:15:54 +0400

    Add JWT middleware

--- containers/api-server/server.js
+++ containers/api-server/server.js
@@ -1,9 +1,11 @@
-const Koa        = require('koa')
-
 const getenv     = require('getenv')
+
+const Koa        = require('koa')
 
 const logger     = require('koa-logger')
 const bodyparser = require('koa-bodyparser')
+
+const jwt        = require('koa-jwt')
 
 const pouchdb    = require('./lib/middlewares/pouchdb')
 const router     = require('./lib/routes')
@@ -11,6 +13,8 @@ const router     = require('./lib/routes')
 // Settings
 
 const PORT = getenv.int('NODE_PORT', 8080)
+
+const JWT_SECRET = getenv('JWT_SECRET')
 
 const SCOPES = [
   'users',
@@ -23,6 +27,8 @@ const app = new Koa()
 
 app.use(logger())
 app.use(bodyparser())
+
+app.use(jwt({ secret: JWT_SECRET }).unless({ path: [/^\/tokens/] }))
 
 app.use(pouchdb(SCOPES))
 app.use(router())
